107f3aeb7eb615074a35237cd53cc558c70f6c2e
Olav Liljeberg <sofnbeat@gmail.com>
2025-11-24 05:21:07 +0100

    Fjernet spesifikke kilder fra SectionSources, beholdt resten

--- src/components/SectionSources.tsx
+++ src/components/SectionSources.tsx
@@ -10,29 +10,15 @@ const SectionSources = () => {
 
 1. [SSB – Prisindeks for brukte boliger](https://www.ssb.no/statbank/table/07241/)
 
-2. [SSB – Boligprisindeks (årlig)](https://www.ssb.no/priser-og-prisindekser/boligpriser/statistikk/boligprisindeksen)
+2. [SSB – Befolkning](https://www.ssb.no/befolkning)
 
-3. [SSB – Befolkning](https://www.ssb.no/befolkning)
+3. [Eiendom Norge – Boligprisstatistikk](https://www.eiendomnorge.no/boligprisstatistikk/)
 
-4. [Eiendom Norge – Boligprisstatistikk](https://www.eiendomnorge.no/boligprisstatistikk/)
+4. [Norges Bank – Styringsrenten](https://www.norges-bank.no/tema/pengepolitikk/styringsrenten/)
 
-5. [Eiendom Norge – Sykepleierindeksen](https://eiendomnorge.no/2023/sykepleierindeksen-2023/)
+5. [Nordea – Nordeas prognose boligpriser 2025](https://www.nordea.com/no/nyheter/mot-lysere-tider-dette-venter-norsk-okonomi-i-2025)
 
-6. [Eiendomsverdi – Sykepleierindeksen (PDF)](https://eiendomsverdi.no/sykepleierindeksen/)
-
-7. [Norges Bank – Styringsrenten](https://www.norges-bank.no/tema/pengepolitikk/styringsrenten/)
-
-8. [DNB – Prognose for boligmarkedet](https://www.dnb.no/dnbnyheter/no/privatoekonomi/dnb-spaar-boligprisene)
-
-9. [Nordea – Nordeas prognose boligpriser 2025](https://www.nordea.com/no/nyheter/mot-lysere-tider-dette-venter-norsk-okonomi-i-2025)
-
-10. [TV2 – First Price-boligmarkedet koker](https://www.tv2.no/nyheter/innenriks/first-price-boligmarkedet-koker/16394877/)
-
-11. [E24 – Panikk i boligmarkedet](https://e24.no/privat/okonomi/i/dwAMgV/boligmarkedet-koker---folk-er-redde-for-aa-henge-etter)
-
-12. [Hjemla – De billigste kommunene i Norge](https://www.hjemla.no/magasin/de-billigste-kommunene-i-norge/)
-
-13. [Krogsveen – Prisstatistikk](https://www.krogsveen.no/boligprisstatistikk)
+6. [Krogsveen – Prisstatistikk](https://www.krogsveen.no/boligprisstatistikk)
 
 **Merk:** Alle kilder er åpne og verifiserbare. Data er hentet fra offentlige rapporter og institusjonelle analyser.
 
